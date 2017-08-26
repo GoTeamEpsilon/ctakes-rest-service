8edee4f52146429b1c1a8be87448256f419029f4
Matthew Vita <matthewvita@live.com>
2017-08-26 13:59:25 -0400

    init

--- README.md
+++ README.md
@@ -1,3 +1,49 @@
+# cTAKES Concept Mention Parser
+
+When given an XMI result from cTAKES, this parser puts together a nice JSON representation of the concept mentions with medical codes and text.
+
+![gif](https://github.com/MatthewVita/cTAKES-Concept-Mention-Parser/blob/master/demo.gif?raw=true)
+
+## INSTALL
+
+```
+$ sudo pip install xmltodict
+$ sudo pip install watchdog
+```
+
+## RUN
+
+```
+$ python parser.py
+$ # place xml file(s) in the data directory (this directory is being watched for additions)
+$ cat data/you-xml-file(s)
+```
+
+## EXAMPLE
+
+As an example, some fake data (credit: https://www.med.unc.edu/medselect/resources/sample-notes/sample-initial-visit-note-1) has been checked into the samples folder to get and idea of the output quality:
+
+> Mr. Smith is a 56-year-old gentleman formally followed at Carolina Premier who presents to obtain a new primary care physician secondary to insurance changes. He has a past medical history significant for a myocardial infarction in 1994. His cholesterol has been fine. Catheterization showed a possible "kink" in one of his vessels and it was thought that he had a possible "eddy" of current which led to a clot. He has been on Coumadin since then as well as a calcium channel blocker with the thought that there may have been a superimposed spasm. He has had several unremarkable stress tests since then. He works out on a Nordic-Track three times a week without any chest pain or shortness of breath. He has a history of possible peptic ulcer disease in 1981. He was treated with H2 blockers and his symptoms resolved. He has never had any bleeding to his knowledge. He had a hernia repair bilaterally in 1989 and surgery for a right knee cyst in 1999, all of which went well. He also has about a year long history of right buttock pain. This happens only when he is sitting for some time and does not change position. It does not happen when he is walking or exercising. He wonders if it might be pyriformis syndrome. If he changes positions frequently or stretches his legs, this seems to help. He has no acute complaints today and is here to get plugged into the system. He does wonder if there is anything else that can be done about his buttock pain.
+
+`$ vi samples/data.json` to see
+
+## TODOS
+
+- Make the lookup O(1) by indexing on XML ID
+- Handle `textsem:MedicationEventMention`
+- Handle `textsem:LabMention`
+- Python3/pep compliance
+- Unit tests
+- Docker container
+- Instructions for setting up the solution as a UIMA pipeline subscriber
+- Code documentation
+- Partition data by coding scheme
+
+## LICENSE
+
+MIT
+
+
 # ctakes-rest-service
 
 ## Description
---
+++ parser.py
@@ -0,0 +1,152 @@
+import json
+import xmltodict
+import sys
+import HTMLParser
+import os
+import time
+import datetime
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+from threading import Thread
+
+class Watcher:
+  DIRECTORY_TO_WATCH = './data'
+
+  def __init__(self):
+    self.observer = Observer()
+
+  def run(self):
+    event_handler = Handler()
+    self.observer.schedule(event_handler, self.DIRECTORY_TO_WATCH, recursive=True)
+    self.observer.start()
+    try:
+      while True:
+        time.sleep(5)
+    except:
+      self.observer.stop()
+      print 'Error'
+
+    self.observer.join()
+
+class Handler(FileSystemEventHandler):
+  @staticmethod
+  def on_any_event(event):
+    if event.is_directory:
+      return None
+    elif event.event_type == 'created' and 'xml' in event.src_path:
+      file_name_without_extension = event.src_path.rsplit('.', 1)[0]
+      c = CodeExtractor(file_name_without_extension)
+      c.start()
+
+
+class CodeExtractor(Thread):
+  DIRECTORY_TO_WATCH = './samples'
+
+  def __init__(self, file):
+    Thread.__init__(self)
+    self.file = file
+    self.converted_data = {}
+    self.concepts = []
+    self.mentions = {}
+    self.MENTION_TYPE = {
+      'MEDICATION_MENTION':       'MedicationMention',
+      'DISEASE_DISORDER_MENTION': 'DiseaseDisorderMention',
+      'PROCEDURE_MENTION':        'ProcedureMention',
+      'SIGN_SYMPTOM_MENTION':     'SignSymptomMention',
+      'ANATOMICAL_SITE_MENTION':  'AnatomicalSiteMention'
+
+      # TODO: handle these addition mentions
+      # 'MEDICATION_EVENT_MENTION': 'MedicationEventMention',
+      # 'LAB_MENTION':              'LabMention'
+    }
+
+  # TODO: Bring in actual logger
+  def log(self, msg):
+    print('[' + self.getName() + '] ' + str(datetime.datetime.utcnow()) + ' ' + msg)
+
+
+  def run(self):
+    self.process_and_output_all_mentions()
+
+
+  def convert_xml_to_dict(self):
+    try:
+      file_handler = open(self.file + '.xml', 'r')
+      self.converted_data = xmltodict.parse(file_handler.read())
+      file_handler.close()
+      return 0
+    except IOError:
+      self.log('Error: File ' + self.file + ' does not appear to exist')
+      return 1
+
+
+  def write_dict_to_json(self):
+    file = open(self.file + '.json', 'w')
+    file.write(json.dumps(self.mentions))
+    file.close()
+
+
+  def extract_all_concepts(self):
+    raw_concepts = self.converted_data['xmi:XMI']['refsem:UmlsConcept']
+
+    for concept in raw_concepts:
+      nullable_code_name = 'unknown'
+      if concept.has_key('@code'):
+        nullable_code_name = concept['@code']
+
+      self.concepts.append({
+        'scheme': concept['@codingScheme'],
+        'id'    : concept['@xmi:id'],
+        'text'  : concept['@preferredText'],
+        'code'  : nullable_code_name
+      })
+
+
+  def map_specific_concept_mention(self, mention_type):
+    if not self.converted_data['xmi:XMI'].has_key('textsem:' + mention_type):
+      self.log('Error: couldn\'t process mention_type ' + mention_type)
+      return
+
+    raw_concept_mentions = self.converted_data['xmi:XMI']['textsem:' + mention_type]
+
+    selected_concept_mentions = []
+    for concept_mention in raw_concept_mentions:
+      for concept_id in concept_mention['@ontologyConceptArr'].split(' '):
+        selected_concept_mentions.append({
+          'id':    concept_id,
+          'begin': concept_mention['@begin'],
+          'end':   concept_mention['@end']
+        })
+
+    self.mentions[mention_type] = []
+    for concept in self.concepts:
+      for selected_concept_mention in selected_concept_mentions:
+        if concept['id'] == selected_concept_mention['id']:
+          concept['begin'] = selected_concept_mention['begin']
+          concept['end'] = selected_concept_mention['end']
+          self.mentions[mention_type].append(concept)
+
+
+  def process_mention(self, mention_type, should_write):
+    self.map_specific_concept_mention(mention_type)
+
+    if should_write:
+      self.write_dict_to_json()
+
+
+  def process_and_output_all_mentions(self):
+    ret_code = self.convert_xml_to_dict()
+    if ret_code == 0:
+      self.log('Started processing')
+      self.extract_all_concepts()
+      for key, value in self.MENTION_TYPE.iteritems():
+        self.process_mention(value, False)
+
+      self.write_dict_to_json()
+      self.log('Completed processing')
+    else:
+      self.log('Error: couldn\'t convert XML to dict')
+
+
+w = Watcher()
+w.run()
