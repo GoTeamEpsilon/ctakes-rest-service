e016af87551d18a362edaced2caa171586d15336
Daniel Ehrlich <daniel.ehrlich1@gmail.com>
2017-09-17 23:05:14 -0500

    separate pre-code into unit tests and integration tests, defined integration test case

--- test.py
+++ test.py
@@ -1,1 +1,20 @@
 import pytest
+from parser import *
+
+###
+### Unit Tests ###
+
+# Test individual parts of parser
+# Handler class
+# CodeExtractor class
+
+
+###
+### Integration Tests ###
+###
+
+# Actually call startParser.py
+# copy data.xml from samples/ to data/
+# sleep for 3 seconds
+# check for presence of new modified file in data/
+# delete file
