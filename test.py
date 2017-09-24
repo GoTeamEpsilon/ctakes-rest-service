0d2e31cb88910679f8bd36a1e9df90942e7a2f24
Daniel Ehrlich <daniel.ehrlich1@gmail.com>
2017-09-24 18:45:52 -0500

    mostly finished end-to-end test

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import pytest
 from parser import *
+import os
 
 ###
 ### Unit Tests ###
@@ -12,9 +13,31 @@ from parser import *
 ###
 ### Integration Tests ###
 ###
+def testParser():
+  # set up paths you need
+  currentPath = os.path.abspath(os.curdir)
+  currentLocation = currentPath + "/samples/data.xml"
+  newLocation = currentPath + "/data/data.xml"
+  print(currentLocation)
+  print(newLocation)
 
-# Actually call startParser.py
-# copy data.xml from samples/ to data/
-# sleep for 3 seconds
-# check for presence of new modified file in data/
-# delete file
+  # move file and run full program
+  w = Watcher()
+  w.run()
+  os.rename(currentLocation, newLocation)
+  ##### # HOW DO I BREAK OUT OF THIS LOOP ^^^^^^
+ 
+  # now check to see if the program converted the file
+  finalResult = currentPath + "/data/data.json"
+  try:
+    assert os.path.isfile(finalResult) == True
+    print("Yay runParser() passed")
+  except AssertionError:
+    print("Sorry assertion error for runParser()")
+
+
+###
+### Run Tests ###
+###
+
+testParser()
