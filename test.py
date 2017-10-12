cfc324b59152c8601c26a7a30290406e5df3d2b6
Daniel Ehrlich <daniel.ehrlich1@gmail.com>
2017-10-11 22:13:12 -0500

    unit test

--- test.py
+++ test.py
@@ -8,37 +8,41 @@ import sys,os
 ### Unit Tests ###
 
 class TestCodeExtractor(unittest.TestCase):
-    # CodeExtractor.__init__ CHECK FOR STATE AT CREATION ACROSS VARIOUS SELF VARIABLES
+
     def setUp(self):
         sys.path.append(os.path.realpath('..'))
-        self.CE = CodeExtractor('samples/data.xml')
+        self.CE = CodeExtractor('samples/data')
 
     def testCodeExtractorInit(self):
         self.assertEqual(self.CE.CONCEPT_MENTION_TYPE['MEDICATION_MENTION'], 'MedicationMention')
 
     def testCodeExtractorLog(self):
         value = self.CE.log('Help I am in trouble!')
-        print("this is: " + value)
+        print("this is: " + value) # debug
         self.assertTrue(type(value) == str)
-        self.assertFalse(len(value) > 10)
+        self.assertTrue(len(value) > 10)
+
+    def testConvertXMLToDict(self):
+        value = self.CE.convert_xml_to_dict()
+        if isinstance(self.CE.converted_data, dict):
+            print("passes")
+            self.assertEqual(value,0)
+
+    ### edit self.file so
+      ### convertxmltodict fails...then check for exception?
 
     def testWriteDictToJsonPass(self):
+        # validate JSON with this https://stackoverflow.com/questions/23344948/python-validate-and-format-json-files
         x = 1
 
     def testWriteDictToJsonFail(self):
         x = 1
 
-# CodeExtractor.convert_xml_to_dict RAISE ERROR
-##pass in a sample corrupt file, perhaps in /sample or just make a whole test folder?
-# CodeExtractor.convert_xml_to_dict RETURN DICTIONARY
-## normal test, does it return 0?
+    ### TEAR DOWN - delete all of the extra test files after running
 
-# CodeExtractor.write_dict_to_json INSPECT FINAL_STRUCTURE VARIABLE FOR JSON-NESS
-## can you get a JSON verifyer or something?
-
-# CodeExtractor.extract_all_unmapped_concept_mentions ???
-# CodeExtractor.map_unmapped_concept_mentions_by_type ???
-# CodeExtractor.process_and_output_all_concept_mentions ???
+    # CodeExtractor.extract_all_unmapped_concept_mentions ???
+    # CodeExtractor.map_unmapped_concept_mentions_by_type ???
+    # CodeExtractor.process_and_output_all_concept_mentions ???
 
 if __name__ == '__main__':
     unittest.main()
